84b30749c920cc919450177398f861eef1697014
Ankuj999 <138982104+Ankuj999@users.noreply.github.com>
2025-07-21 11:49:11 +0530

    Update Certifications.py

--- pages/Certifications.py
+++ pages/Certifications.py
@@ -6,9 +6,9 @@ st.subheader("📜 Certifications")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/FG.jpg", caption="Flipkart Grid 6.0", use_column_width=True)
+    st.image("images/FG.jpg", caption="Flipkart Grid 6.0", use_container_width=True)
     st.markdown("**Flipkart Grid 5.0 - Software Development Challenge**")
 
 with col2:
-    st.image("images/Deloitte.jpg", caption="Forage Virtual Internship", use_column_width=True)
+    st.image("images/Deloitte.jpg", caption="Forage Virtual Internship", use_container_width=True)
     st.markdown("**Data Analyst Virtual Internship – Forage (Deloitte)**")
